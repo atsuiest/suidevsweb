34642c43d36e0d14555525224694109e1de98326
Luis Bascur <luis.bascur89@gmail.com>
2019-01-22 05:23:30 -0300

    Schooltime added

---
+++ clients/schooltime/precache-manifest.4116d61d2db7af9b8e0ddb6ca5b79bed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "12b97607e0a126d5577263b485eba75b",
+    "url": "/static/media/jockey.12b97607.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d16d1cc866eaba418ee1",
+    "url": "/static/js/main.d16d1cc8.chunk.js"
+  },
+  {
+    "revision": "359ce44c4f0bdb4f7dbf",
+    "url": "/static/js/1.359ce44c.chunk.js"
+  },
+  {
+    "revision": "d16d1cc866eaba418ee1",
+    "url": "/static/css/main.5661b20c.chunk.css"
+  },
+  {
+    "revision": "a1e176531b398fdcef387b442a64c589",
+    "url": "/index.html"
+  }
+];
